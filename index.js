10fd40cab6f47692fa05a76e23db88ca79286c75
kidouchi <kidouchi@andrew.cmu.edu>
2015-08-28 23:37:45 -0400

    changed header

--- index.js
+++ index.js
@@ -13,4 +13,33 @@ $(document).ready(function() {
         }
       });
     });
+    
+    $(function() {
+        var $el, leftPos, newWidth,
+            $mainNav = $(".header-items");
+
+        $mainNav.append("<li id='magic-line'></li>");
+        var $magicLine = $("#magic-line");
+
+        $magicLine
+            .width($(".current-item").width())
+            .css("left", $(".current-item a").position().left)
+            .data("origLeft", $magicLine.position().left)
+            .data("origWidth", $magicLine.width());
+
+        $(".header-items li a").hover(function() {
+            $el = $(this);
+            leftPos = $el.position().left;
+            newWidth = $el.parent().width();
+            $magicLine.stop().animate({
+                left: leftPos,
+                width: newWidth
+            });
+        }, function() {
+            $magicLine.stop().animate({
+                left: $magicLine.data("origLeft"),
+                width: $magicLine.data("origWidth")
+            });    
+        });
+    });
 });
